ef0d4adf081dfe8f1f4e7dfb86bcf7c1b22927ec
Sīlinda <silinda.gercane@gmail.com>
2021-08-27 21:12:44 +0300

    Some upgrades

--- sildapiano/sildapiano/public/forms.py
+++ sildapiano/sildapiano/public/forms.py
@@ -3,12 +3,13 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
 class SignUpForm(UserCreationForm):
-    first_name = forms.CharField(label='Vārds', max_length=50, required=True, help_text='*')
-    last_name = forms.CharField(label='Uzvārds', max_length=50, required=True, help_text='*')
-    email = forms.EmailField(label='E-pasts', max_length=110, required=True, help_text='*')
-    phone_num = forms.CharField(label='Telefona numurs', max_length=100, required=True, help_text='*')
+    first_name = forms.CharField(label='Vārds', max_length=50, required=True)
+    last_name = forms.CharField(label='Uzvārds', max_length=50, required=True)
+    email = forms.EmailField(label='E-pasts', max_length=110, required=True,
+                             widget=forms.EmailInput(attrs={'class': 'form-control'}))
+    phone_num = forms.CharField(label='Telefona numurs', max_length=100, required=True)
     birth_date = forms.DateField(label='Dzimšanas diena', required=False, help_text=
-    'GGGG-MM-DD Ja gribi labāk iepazīties un saņemt par vienu dzimšanas dienas apveikumu vairāk! :)')
+    'GGGG-MM-DD')
     description = forms.CharField(label='Tavs raksturojums', max_length=1000, required=False,
                                   help_text='Pastāsti man par sevi, ja vēlies! :)')
 
